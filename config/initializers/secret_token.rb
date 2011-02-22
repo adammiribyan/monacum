771e16eac06730b8e4bf5362ad986e394bd31a1d
Adam Miribyan <kreditmarket.com@gmail.com>
2011-02-22 18:18:41 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Monacum::Application.config.secret_token = '9a3970c189b16177e625e626cf631a395a03b1ed3753dea5299dcdd43950aa952f3a85e2c7dde5d397a114c0ef101e72a8640300bdeaea942fc7f4f014992f66'
