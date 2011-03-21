23eba651e756b9af93cf8f8a1031428f3fcd1adf
Adam Miribyan <kreditmarket.com@gmail.com>
2011-03-21 14:57:08 +0300

    client sidebar data

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -1,2 +1,10 @@
+# encoding: utf-8
+
 module ClientsHelper
+  def dependents_count(object)
+    if object and object.personal_izhd_count > 0
+      "#{object.personal_izhd_count} #{Russian.p(object.personal_izhd_count, "иждивенец", "иждивенца", "иждивенцев")}"
+    end
+  end
+  
 end
