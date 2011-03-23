3c112a5967d95d158edeb1807f09e22ccb2d39f6
Adam Miribyan <kreditmarket.com@gmail.com>
2011-03-23 17:50:01 +0300

    client facebox

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -7,4 +7,24 @@ module ClientsHelper
     end
   end
   
+  # ENHANCE
+  def client_address(client, type)
+    case type
+    when "current"
+      city ||= "г. #{client.fact_city}, " if client.fact_city.present?
+      street ||= "#{client.fact_street}, " if client.fact_street.present?
+      house ||= "дом #{client.fact_house}, " if client.fact_house.present?
+      building ||= "#{client.fact_building}, " if client.fact_building.present?
+      flat ||= "кв. #{client.fact_flat}" if client.fact_flat.present?
+    when "registered"
+      city ||= "г. #{client.reg_city}, " if client.reg_city.present?
+      street ||= "#{client.reg_street}, " if client.reg_street.present?
+      house ||= "дом #{client.reg_house}, " if client.reg_house.present?
+      building ||= "#{client.reg_building}, " if client.reg_building.present?
+      flat ||= "кв. #{client.reg_flat}" if client.reg_flat.present?
+    end
+    
+    "<span>#{city}</span><br /><span>#{street}</span><br /><span>#{house}</span><span>#{building}</span><span>#{flat}</span>".html_safe
+  end
+  
 end
